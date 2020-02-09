1ce42e7730840024e45439a11aa1524bf37c371e
Gustavo Junior de Oliveira Martins <gustavojunior37@hotmail.com>
2020-02-09 14:45:29 -0200

    Configuração BD

---
+++ CrudUsuarios/CrudUsuarios/Models/Context/Context.cs
@@ -0,0 +1,15 @@
+﻿using CrudUsuarios.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudUsuarios.Models.Context
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
--- CrudUsuarios/CrudUsuarios/Startup.cs
+++ CrudUsuarios/CrudUsuarios/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CrudUsuarios.Models.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace CrudUsuarios
 {
@@ -25,7 +20,8 @@ namespace CrudUsuarios
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddDbContext<Context>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
