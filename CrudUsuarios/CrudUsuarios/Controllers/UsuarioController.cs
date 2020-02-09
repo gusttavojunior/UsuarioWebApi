017c47a78081ba5194e7b695518151d30a24bd4e
Gustavo Junior de Oliveira Martins <gustavojunior37@hotmail.com>
2020-02-09 20:48:55 -0200

    2

---
+++ CrudUsuarios/CrudUsuarios/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CrudUsuarios.Models.Context;
+using CrudUsuarios.Models.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudUsuarios.Controllers
+{
+    [Route("api/usuario")]
+    [ApiController]
+    public class UsuarioController : Controller
+    {
+        private readonly Context _context;
+        public UsuarioController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("listar")]
+        public List<Usuario> Listar()
+        {
+            var lista = _context.Usuarios.ToList();
+            return lista;
+        }
+
+
+        [HttpPost("inserir")]
+        public string Inserir(Usuario usuario)
+        {
+            if(usuario != null)
+            {
+                _context.Usuarios.Add(usuario);
+                _context.SaveChanges();
+
+                return "Usuário Cadastrado";
+            }
+
+            return "Usuário não cadastrado";
+        }
+
+
+
+    }
+}
